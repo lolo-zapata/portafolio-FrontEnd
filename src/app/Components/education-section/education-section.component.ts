48bdc6ae0f4a50936a08ac6aeab336de25f0c176
lolo-zapata <lorenzozapazarate@gmail.com>
2022-05-21 12:18:03 -0300

    master

--- src/app/Components/education-section/education-section.component.ts
+++ src/app/Components/education-section/education-section.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/Servicios/portfolio.service';
 
 @Component({
   selector: 'app-education-section',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./education-section.component.css']
 })
 export class EducationSectionComponent implements OnInit {
+  educationList:any;
 
-  constructor() { }
+  constructor(private datosPortfolio:PortfolioService) { }
 
   ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe(data=>{
+      console.log(data);
+      this.educationList=data.educacion;
+    })
   }
 
 }
